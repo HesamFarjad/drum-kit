87cdf32c815c17409efdbcd5a01babd4ae593d70
Hesam Farjad <81914229+HesamFarjad@users.noreply.github.com>
2024-02-29 08:42:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var numberOfButton = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    makeSound(this.innerHTML);
+    applyAnimatoin(this.innerHTML);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  applyAnimatoin(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log("This key is not defined");
+  }
+}
+
+function applyAnimatoin(key) {
+  var addressBtn = "." + key;
+  var activeBtn = document.querySelector(addressBtn);
+  activeBtn.classList.add("pressed");
+
+  setTimeout(function () {
+    activeBtn.classList.remove("pressed");
+  }, 100);
+}
